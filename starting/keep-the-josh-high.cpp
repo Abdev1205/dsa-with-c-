379fbd58381d05e2016ca957527116e0ade91c13
Abhay1205Mishra <97285855+Abhay1205Mishra@users.noreply.github.com>
2022-11-12 00:56:39 +0530

    intial commit

---
+++ starting/keep-the-josh-high.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main() {
+    string a ,b;
+    cout<< "Do you want to improve yourself"<<endl;
+    cin>>a;
+    cout<< "will you do coding everyday"<<endl;
+    cin>>b;
+    cout<<a+b;
+
+}
